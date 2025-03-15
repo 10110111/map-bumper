a32fb965866252a18b41a813a7b1bb669f59ed28
Ruslan Kabatsayev <b7.10110111@gmail.com>
2025-03-15 22:11:29 +0800

    combine-lroc-and-moonkit: fix the look of the south pole

--- combine-lroc-and-moonkit.cpp
+++ combine-lroc-and-moonkit.cpp
@@ -51,10 +51,12 @@ try
     const uchar* lrocData = lroc.bits() + lrocShift * lrocRowStride;
     const auto polesLowerBorder = (90-70)/180.*moonkit.height();
     const auto polesUpperBorder = polesLowerBorder + lroc.height() - lrocShift;
+
     for(int j = 0; j < H; ++j)
     {
-        if(j < polesLowerBorder || j >= polesUpperBorder)
+        if(j < polesLowerBorder)
         {
+            // North pole
             for(int i = 0; i < W3; i += 3)
             {
                 constexpr double shiftR = 0.75182503818554 , coefR = -0.0523835897824206;
@@ -69,6 +71,21 @@ try
                 data[i + 0] = sRGBTransferFunction(r) * 255;
                 data[i + 1] = sRGBTransferFunction(g) * 255;
                 data[i + 2] = sRGBTransferFunction(b) * 255;
+            }
+        }
+        else if(j >= polesUpperBorder)
+        {
+            // South pole
+            for(int i = 0, k = 0; i < W3; i += 3, ++k)
+            {
+                constexpr double shift = 0.603591562174297, coef = -0.0416058207644435, power = -4.4;
+                const double valMK = data[i+1] / 255.;
+                const double Y = std::max(0.24, shift + coef * std::pow(valMK, power));
+                const double r0 = 1.26443109577546, g0 = 0.950351679150597, b0 = 0.712763115940595;
+                const double r = r0 * Y, g = g0 * Y, b = b0 * Y;
+                data[i + 0] = sRGBTransferFunction(std::clamp(r, 0., 1.)) * 255;
+                data[i + 1] = sRGBTransferFunction(std::clamp(g, 0., 1.)) * 255;
+                data[i + 2] = sRGBTransferFunction(std::clamp(b, 0., 1.)) * 255;
             }
         }
         else
